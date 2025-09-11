c371b16180affb6f92b81b19fb8e7f0f0353261b
Santiago Papale <166446295+SPapale@users.noreply.github.com>
2025-09-11 14:40:04 -0300

    agrego fondo en movimiento y a walter

--- SuperPablo/src/gui/Jugador.java
+++ SuperPablo/src/gui/Jugador.java
@@ -1,53 +1,57 @@
 package gui;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Color;
 
-import javax.swing.JPanel;
+public class Jugador {
+    private int x, y;
+    private int ancho, alto;
+    private int velocidadX = 5;
 
-public class Jugador extends JPanel {
+    // Para salto
+    private int velocidadY = 0;
+    private final int gravedad = 1;
+    private boolean enSuelo = true;
 
-	private int ancho=50;
-	private int alto=20;
-	private int x, y;
-	
-	
-	public Jugador(int x, int y) {
-		this.x =x;
-		this.y =y;
-	}
-	
-	public void dibujar(Graphics g)
-	{
-			g.setColor(Color.GREEN);
-			g.fillRect(x, y, ancho, alto);
-	}
-	
-	public void moverDerecha()
-	{
-		x+=4;
-	}
-	
-	public void moverIzquierda()
-	{
-		x-=4;
-	}
-	
-	public void moverArriba()
-	{
-		y+=4;
-	}
-	
-	public void moverAbajo()
-	{
-		y-=4;
-	}
-	
+    public Jugador(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.ancho = 50;
+        this.alto = 50;
+    }
 
-	protected void superPaintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		g.setColor(Color.GREEN);
-		g.fillRect(500, 50, 50, 20);
-	}
+    // Movimiento horizontal
+    public void moverDerecha() { x += velocidadX; }
+    public void moverIzquierda() { x -= velocidadX; }
+
+    // Saltar
+    public void saltar() {
+        if (enSuelo) {
+            velocidadY = -17;  // fuerza del salto
+            enSuelo = false;
+        }
+    }
+
+    // Actualizar posición vertical
+    public void actualizar(int sueloY) {
+        if (!enSuelo) {
+            y += velocidadY;
+            velocidadY += gravedad;  // aplica gravedad
+
+            if (y >= sueloY) {
+                y = sueloY;
+                velocidadY = 0;
+                enSuelo = true;
+            }
+        }
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, ancho, alto);
+    }
+    
 }
